af0116ceaff6369d123e95dd72d810f3909e04bb
Yurii Derevych <gkhelloworld@gmail.com>
2020-01-16 22:08:52 +0200

    0.2.6

--- table.js
+++ table.js
@@ -1,18 +1,17 @@
 const data = require('./map')
 const fs = require('fs')
 
-// Convert to readme file
-
 const keys = Object.keys(data)
 
 let readme = `## List of ISO 639-1 codes
 ### List of language names ordered by code
-|  Code  |  Name  |  Native  |  Dir  |
-|--------|--------|----------|-------|
+|  #  |  Code  |  Name  |  Native  |  Dir  |
+|-----|--------|--------|----------|-------|
 `
-
+let i = 0
 keys.forEach((key) => {
-  readme += `| ${key} | ${data[key].name} | ${data[key].native} | ${(Number(data[key].dir) === 1) ? 'ltr' : 'rtl'} |\n`
+  i++
+  readme += `| ${i} | ${key} | ${data[key].name} | ${data[key].native} | ${(Number(data[key].dir) === 1) ? 'ltr' : 'rtl'} |\n`
 })
 
 fs.writeFile('languages-list.md', readme, () => {
